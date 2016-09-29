530d27c327a22639a63455796500d0d0a8c48a2f
Stacy Wong <ninjastacyla@gmail.com>
2016-09-28 21:58:18 -0700

    Add files via upload

---
+++ aboutUsscript.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function() {
+    jQuery('.fellowInfo').addClass("hidden2").viewportChecker({
+        classToAdd: 'visible2 animated fadeIn',
+        offset: 100
+    });
+});
+
+jQuery(document).ready(function() {
+    jQuery('#fellowshipHeader').addClass("hidden2").viewportChecker({
+        classToAdd: 'visible2 animated bounce',
+        offset: 100
+    });
+});
